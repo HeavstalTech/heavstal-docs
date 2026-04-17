202e76ceb6cbb5411709455c996393cb581f2207
HEAVSTAL TECH™ <ekivererepromise@gmail.com>
2026-04-17 16:16:05 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,22 @@ import starlight from '@astrojs/starlight';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://docs.heavstal.com',
+  site: 'https://heavstal-docs.vercel.app',
   integrations: [
     starlight({
       title: 'Heavstal Docs',
-      customCss: [
-        './src/tailwind.css',
-      ],
+      customCss: ['./src/tailwind.css'],
       sidebar: [
-        { label: 'Introduction', link: '/' }
+        {
+          label: 'Getting Started',
+          link: '/',
+        },
+        {
+          label: 'Heavstal APIs',
+          autogenerate: { directory: 'apis' },
+        }
       ],
     }),
-    tailwind({
-      // Prevents Tailwind from overwriting Starlight's default beautiful styles
-      applyBaseStyles: false,
-    }),
+    tailwind({ applyBaseStyles: false }),
   ],
 });
