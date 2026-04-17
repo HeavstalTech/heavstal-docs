2fd00495234d2cb5b05bd0beb4110a4dd54aa6c1
HEAVSTAL TECH™ <ekivererepromise@gmail.com>
2026-04-17 22:31:37 +0100

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,10 @@
 import { defineCollection } from 'astro:content';
-import { docsLoader, docsSchema } from '@astrojs/starlight/schema';
+import { docsSchema } from '@astrojs/starlight/schema';
+import { glob } from 'astro/loaders';
 
 export const collections = {
   docs: defineCollection({
-    loader: docsLoader(),
+    loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/docs' }),
     schema: docsSchema(),
   }),
 };
