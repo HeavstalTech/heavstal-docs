d745dc2fde0f1099353596289756e1e9969d0289
HEAVSTAL TECH™ <ekivererepromise@gmail.com>
2026-07-24 14:47:18 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
       },
       head:[
         { tag: 'link', attrs: { rel: 'apple-touch-icon', href: '/favicon.ico' } },
-        { tag: 'meta', attrs: { property: 'og:image', content: 'https://docs.heavatal.com.ng/social-banner.png' } },
+        { tag: 'meta', attrs: { property: 'og:image', content: 'https://docs.heavstal.com.ng/social-banner.png' } },
         { tag: 'meta', attrs: { property: 'og:type', content: 'website' } },
         { tag: 'meta', attrs: { name: 'twitter:card', content: 'summary_large_image' } },
         { tag: 'meta', attrs: { name: 'twitter:image', content: 'https://docs.heavstal.com.ng/social-banner.png' } },
@@ -31,22 +31,27 @@ export default defineConfig({
         },
         {
           label: 'Heavstal APIs',
+          collapsed: true,
           autogenerate: { directory: 'apis' },
         },
         {
           label: 'Heavstal Auth Provider',
+          collapsed: true,
           autogenerate: { directory: 'oauth' },
         },
         {
           label: 'NPM Libraries (Modules)',
+          collapsed: true,
           autogenerate: { directory: 'modules' },
         },
         {
-          label: 'Heavstal Bots (Wa/Tg)', // I also fixed a small typo here ('Heavatal' -> 'Heavstal')
+          label: 'Heavstal Bots (Wa/Tg)', 
+          collapsed: true,
           autogenerate: { directory: 'bots' },
         },
         {
           label: 'Heavstal Tech Ecosystem Documentary',
+          collapsed: true,
           items:[
             {
               label: 'Overview',
@@ -54,12 +59,14 @@ export default defineConfig({
             },
             {
               label: 'Heavstal Tech Platform',
+              collapsed: true,
               items:[
                 { label: 'Introduction', link: '/guides/heavstal-tech/' },
               ]
             },
             {
               label: 'Heavstal Bots',
+              collapsed: true,
               items:[
                 { label: 'Introduction', link: '/guides/heavstal-bots/' },
                 { label: 'How to Make an Authcode', link: '/guides/heavstal-bots/auth-code/' },
@@ -69,6 +76,7 @@ export default defineConfig({
             },
             {
               label: 'Heavstal Accounts',
+              collapsed: true,
               items:[
                 { label: 'Introduction', link: '/guides/heavstal-accounts/' },
               ]
