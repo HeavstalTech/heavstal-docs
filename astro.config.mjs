f132a9fd8ec8f97201f64fe3835124f517e6f973
HEAVSTAL TECH™ <ekivererepromise@gmail.com>
2026-08-15 22:33:52 +0100

    Comment out Tailwind CSS import
    Comment out the Tailwind CSS import in the configuration file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config'
 import starlight from '@astrojs/starlight'
 import react from '@astrojs/react'
 import mdx from '@astrojs/mdx'
-//import tailwind from '@tailwindcss/vite'
+///import tailwind from '@tailwindcss/vite'
 
 export default defineConfig({
   site: 'https://docs.heavstal.com.ng',
