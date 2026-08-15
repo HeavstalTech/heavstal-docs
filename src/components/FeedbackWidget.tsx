2d7d41e9053fca07643d642aab05f1624525d71b
HEAVSTAL TECH™ <ekivererepromise@gmail.com>
2026-08-15 12:09:12 +0100

    Import React alongside useState in FeedbackWidget

--- src/components/FeedbackWidget.tsx
+++ src/components/FeedbackWidget.tsx
@@ -1,5 +1,5 @@
 //src/components/FeedbackWidget.tsx
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { ThumbsUp, ThumbsDown } from 'lucide-react';
 import toast, { Toaster } from 'react-hot-toast';
 
