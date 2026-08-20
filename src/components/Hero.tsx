6da5883d031615887d96a794065089bbb7df63c7
HEAVSTAL TECH™ <ekivererepromise@gmail.com>
2026-08-20 19:01:52 +0100

    Remove design constraint note from Hero component
    Removed note about design constraints.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,5 @@
 // Docs/src/components/Hero.tsx
 import React from 'react';
-// Note: every shape, size, layout etc. that is here was made to be perfect, changing the even a single margin may break the design 
 
 export default function Hero() {
   return (
