9573b208e3c7e11e53d7633ea32709a4c0c68815
HEAVSTAL TECH™ <ekivererepromise@gmail.com>
2026-08-20 13:09:29 +0100

    Update docs schema with additional options
    Extended the docsSchema to include hideTitle and hidePagination options.

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,10 +1,16 @@
-import { defineCollection } from 'astro:content';
+// Docs/src/content.config.ts
+import { defineCollection, z } from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
 import { glob } from 'astro/loaders';
 
 export const collections = {
   docs: defineCollection({
     loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/docs' }),
-    schema: docsSchema(),
+    schema: docsSchema({
+      extend: z.object({
+        hideTitle: z.boolean().default(false),
+        hidePagination: z.boolean().default(false),
+      })
+    }),
   }),
 };
