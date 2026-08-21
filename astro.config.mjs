4425ba1efeac8067816e7baeda34f3001c61c991
HEAVSTAL TECH™ <ekivererepromise@gmail.com>
2026-08-21 17:43:27 +0100

    Add Icon component to configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,6 +22,7 @@ export default defineConfig({
         Pagination: './src/components/Feedback.astro',
         PageTitle: './src/components/PageTitleOverride.astro',
         PageFrame: './src/components/PageFrameOverride.astro',
+        Icon: './src/components/IconOverride.astro',
       },
       head:[
         { tag: 'link', attrs: { rel: 'apple-touch-icon', href: '/favicon.ico' } },
